56fed634c7ef1b8568d56d10321bd644c9499eac
Saba Silagadze <sbsilagadze@gmail.com>
2025-01-01 14:24:49 +0400

    feat: disable eslint build errors

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [{ hostname: 'static-00.iconduck.com' }, { hostname: 's3.amazonaws.com' }, { hostname: 'images.unsplash.com' }]
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   }
 };
 
