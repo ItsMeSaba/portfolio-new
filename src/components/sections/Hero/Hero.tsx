3c63f138141fa951d3020e54198f87664547e3c5
Saba Silagadze <sbsilagadze@gmail.com>
2025-03-15 00:37:12 +0400

    feat: update Hero component styling and particle effects for improved visual appeal

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -26,20 +26,22 @@ export function Hero() {
       {isMobile === true && (
         <Vortex
           rangeY={250}
-          particleCount={500}
+          particleCount={400}
           baseRadius={1}
           rangeRadius={1}
         />
       )}
-      {isMobile === false && <Vortex rangeY={150} particleCount={700} />}
+      {isMobile === false && <Vortex rangeY={150} particleCount={800} />}
+
+      <div className="absolute inset-0 bg-black/5 backdrop-blur-[2px] z-[10]" />
 
       <div className="relative z-10 flex items-center justify-center w-full h-full">
         <div className="flex flex-col gap-1">
-          <p className="text-xl md:text-3xl mx-auto transparency w-fit font-semibold py-[2px] px-2">
+          <p className="text-xl md:text-3xl backdrop-blur-[30px] rounded-md overflow-hidden mx-auto w-fit font-semibold py-[2px] px-2">
             Hi! I'm
           </p>
 
-          <div className="text-[50px] transparency w-fit font-semibold py-[2px] px-4">
+          <div className="text-[50px] w-fit backdrop-blur-[30px] rounded-md overflow-hidden font-semibold py-[2px] px-4">
             <h1 className="text-3xl md:text-5xl">Saba Silagadze</h1>
           </div>
         </div>
