50f143f537d6f7c2be218e685bd7a71398ca0334
Saba Silagadze <sbsilagadze@gmail.com>
2025-10-14 16:42:49 +0400

    feat: moves summary in hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
 import { Hero } from "@/components/sections/hero/Hero";
-import { Text } from "@/components/sections/text/Text";
-import { Experience } from "@/components/sections/experience/Experience";
 import { Projects } from "@/components/sections/projects/Projects";
 import { TechStack } from "@/components/sections/tech-stack/TechStack";
-import { LinkedinRecommendations } from "@/components/sections/linkedin-recommendations/LinkedinRecommendations";
+import { Experience } from "@/components/sections/experience/Experience";
 import { ContactMe } from "@/components/sections/contact-me/ContactMe";
 import { bejamasProjects } from "@/components/sections/projects/bejamas-projects";
 import { personalProjects } from "@/components/sections/projects/personal-projects";
+import { LinkedinRecommendations } from "@/components/sections/linkedin-recommendations/LinkedinRecommendations";
 
 export default function Home() {
   return (
     <div className="min-h-screen relative font-[family-name:var(--font-geist-sans)]">
       <main className="w-full min-h-screen">
         <Hero />
-
-        <Text
-          id="about"
-          words="Frontend-focused JavaScript engineer with 5+ years of experience building fast, accessible, and SEO-friendly web apps for 100k+ monthly users. Strong foundation in React/Next.js and CMS integration, with additional experience implementing AI-powered features using OpenAI APIs, LangChain, and vector databases. Comfortable owning features end-to-end, from frontend to backend APIs and cloud deployments."
-        />
 
         <Experience id="experience" />
 
--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -1,22 +1,37 @@
 import clsx from "clsx";
+import { Text } from "@/components/sections/text/Text";
 import { ModelViewer } from "../../modules/model-viewer/ModelViewer";
 
 export function Hero() {
   return (
-    <section className="min-h-[95vh] md:min-h-[100dvh] relative select-none grid grid-rows-[3fr_1fr] md:grid-rows-1 grid-cols-1 md:grid-cols-2 items-center">
+    <section className="min-h-[95vh] md:min-h-[100dvh] relative select-none grid grid-rows-[1fr_1fr] md:grid-rows-1 grid-cols-1 md:grid-cols-2 items-center">
       <ModelViewer />
 
       <div className="relative z-10 items-start justify-start md:justify-center w-full h-full flex flex-col pl-[5%]">
-        <p
-          className={
-            "text-xl md:text-3xl backdrop-blur-[30px] rounded-md w-fit font-semibold py-[2px]"
-          }
-        >
+        <p className={"text-lg md:text-2xl rounded-md w-fit md:mb-1"}>
           Hi! I'm
         </p>
 
-        <div className="text-[50px] w-fit backdrop-blur-[30px] rounded-md font-semibold py-[2px]">
-          <h1 className="text-3xl md:text-5xl">Saba Silagadze</h1>
+        <div className="text-[50px] w-fit rounded-md font-semibold py-[2px]">
+          <h1 className="text-3xl md:text-5xl mb-2 md:mb-6">Saba Silagadze</h1>
+
+          <div className="text-sm md:text-lg max-w-[600px] pr-4 font-normal text-balance">
+            <p>
+              Frontned Focused Full Stack Developer with 5+ years of experience
+              building high-performance, SEO-optimized, and scalable web
+              applications for international clients across Europe, Asia, and
+              the U.S.
+            </p>
+
+            <br />
+
+            <p>
+              Skilled in React, Next.js, Node.js, and TypeScript with strong
+              focus on performance optimization, API design, and cloud
+              deployment. Delivered 15+ projects, including 7 large-scale
+              platforms at Bejamas.
+            </p>
+          </div>
         </div>
       </div>
     </section>
